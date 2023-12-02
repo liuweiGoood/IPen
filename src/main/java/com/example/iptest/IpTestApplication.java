4a14bcc999af0e0b42e1b8538454ea32fa7ea92c
liuweiGoood <792745464@qq.com>
2023-12-02 17:35:41 +0800

    first commit

---
+++ src/main/java/com/example/iptest/IpTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.iptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iptest/IpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
